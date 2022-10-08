bf4320c2df4b6f66268f5151b8a638aa87ba4a8f
zorlikowski <115358253+zorlikowski@users.noreply.github.com>
2022-10-08 16:47:49 +0200

    zad4 update

--- zad4.cpp
+++ zad4.cpp
@@ -13,6 +13,8 @@ int main() {
   cout << "Podaj trzecią liczbę całkowitą: " << endl;
   cin >> numb3;
 
+
+//sposób 1
   if(numb1 > numb2 && numb1 > numb3){
     cout << "Najwiękasza liczba to: " << numb1;
   }
@@ -22,5 +24,37 @@ int main() {
    else if (numb3 > numb2 && numb3 > numb1){
     cout << "Najwiękasza liczba to: " << numb3;
    }
+
+
+//sposób 2
+   if(numb1 > numb2){
+    if(numb1 > numb3){
+        cout << "Najwiękasza liczba to: " << numb1;
+    }
+   }
+    else if(numb2 > numb1){
+    if(numb2 > numb3){
+        cout << "Najwiękasza liczba to: " << numb2;
+    }
+   }else if(numb3 > numb1){
+    if(numb3 > numb2){
+        cout << "Najwiękasza liczba to: " << numb3;
+    }
+   }
+ 
+    //sposób 3
+    if(numb1 > numb2 > numb3){
+         cout << "Najwiękasza liczba to: " << numb1;
+    }else if(numb1 > numb3 > numb2){
+        cout << "Najwiękasza liczba to: " << numb1;
+    }else if(numb2 > numb3 > numb1){
+        cout << "Najwiękasza liczba to: " << numb2;
+    }else if(numb2 > numb1 > numb3){
+        cout << "Najwiękasza liczba to: " << numb2;
+    }else if(numb3 > numb1 > numb2){
+        cout << "Najwiękasza liczba to: " << numb3;
+    }else if(numb3 > numb2 > numb1){
+        cout << "Najwiękasza liczba to: " << numb3;
+    }
   return 0;
 }
